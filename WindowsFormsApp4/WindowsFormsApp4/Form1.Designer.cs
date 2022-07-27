9d55370b0fe8891c4f89a3e72c975a0aca0050b9
Onat Yılmaz <92721509+OnatYilmazz@users.noreply.github.com>
2022-07-27 09:26:16 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 183);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1446, 491);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.ForeColor = System.Drawing.Color.Navy;
+            this.button1.Location = new System.Drawing.Point(33, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.ForeColor = System.Drawing.Color.Navy;
+            this.button2.Location = new System.Drawing.Point(33, 98);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.ForeColor = System.Drawing.Color.Navy;
+            this.button3.Location = new System.Drawing.Point(33, 142);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(268, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SİL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(807, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(807, 44);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(271, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(807, 72);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(271, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(807, 101);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(271, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(714, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TC Kimlik No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(714, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(714, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Telefon No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Açıklama";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(714, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Dosya No";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(33, 15);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(268, 36);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "ARAMA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(807, 127);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(271, 44);
+            this.textBox6.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1114, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Sıra No";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1208, 15);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(271, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1114, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 17);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Kart Tipi";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1208, 44);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(271, 22);
+            this.textBox8.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1114, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 17);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Kartı Basan";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1114, 101);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 17);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Kayıt Yapan";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1208, 71);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(271, 22);
+            this.textBox9.TabIndex = 24;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(1208, 98);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(271, 22);
+            this.textBox10.TabIndex = 25;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1114, 130);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 17);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Kayıt Tarihi";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1208, 127);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(271, 22);
+            this.dateTimePicker1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1507, 686);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+    SqlConnection con;
+    SqlDataAdapter da;
+    SqlCommand cmd;
+    DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       void griddoldur()
+       {
+            con = new SqlConnection("Data Source=DESKTOP-BAV2LIP\\SQLEXPRESS;Initial Catalog=DENEME;Integrated Security=True");
+            da = new SqlDataAdapter("Select *From kisi", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds,"kisi");
+            dataGridView1.DataSource = ds.Tables["kisi"];
+            con.Close();
+       }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox5.Text == "" || textBox7.Text == "" || textBox8.Text == "" || textBox10.Text == "")
+            {
+                if (textBox1.Text == "")
+                    MessageBox.Show("TC Kimlik Numarası eksik.");
+                if (textBox2.Text == "")
+                    MessageBox.Show("Ad Soyad eksik.");
+                if (textBox5.Text == "")
+                    MessageBox.Show("Dosya numarası eksik.");
+                if (textBox7.Text == "")
+                    MessageBox.Show("Sıra numarası eksik.");
+                if (textBox8.Text == "")
+                    MessageBox.Show("Kart Tipi eksik.");
+                if (textBox10.Text == "")
+                    MessageBox.Show("Kayıt Yapan kişi eksik.");
+                goto Bitis;
+            }
+            else
+            {
+                if (textBox6.Text.Length > 254)
+                {
+                    MessageBox.Show("Açıklama 255 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox1.Text.Length != 11)
+                {
+                    MessageBox.Show("TC Kimlik Numarası 11 karakter olmak zorunda.");
+                    goto Bitis;
+                }
+                if (textBox2.Text.Length > 49)
+                {
+                    MessageBox.Show("Ad Soyad 50 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox3.Text.Length > 19)
+                {
+                    MessageBox.Show("Telefon Numarası 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox5.Text.Length > 19)
+                {
+                    MessageBox.Show("Dosya Numarası 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox7.Text.Length > 19)
+                {
+                    MessageBox.Show("Sıra Numarası 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox8.Text.Length > 19)
+                {
+                    MessageBox.Show("Kart Tipi 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox9.Text.Length > 19)
+                {
+                    MessageBox.Show("Kartı Basan kişi 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                if (textBox10.Text.Length > 19)
+                {
+                    MessageBox.Show("Kayıt yapan kişi 20 karakterden fazla olamaz.");
+                    goto Bitis;
+                }
+                goto Insert;
+            }
+            Insert: {
+                string sorgu = "Insert into kisi (no,adsoyad,tel,dosya,aciklama,sira,kart,basankisi,kayitkisi,tarih) values (@no,@adsoyad,@tel,@dolap,@dosya,@aciklama,@sira,@kart,@basankisi,@kayitkisi,@tarih)";
+                cmd = new SqlCommand(sorgu, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@no", textBox1.Text);
+                cmd.Parameters.AddWithValue("@adsoyad", textBox2.Text);
+                cmd.Parameters.AddWithValue("@tel", textBox3.Text);
+                cmd.Parameters.AddWithValue("@dosya", textBox5.Text);
+                cmd.Parameters.AddWithValue("@aciklama", textBox6.Text);
+                cmd.Parameters.AddWithValue("@sira", textBox7.Text);
+                cmd.Parameters.AddWithValue("@kart", textBox8.Text);
+                cmd.Parameters.AddWithValue("@basankisi", textBox9.Text);
+                cmd.Parameters.AddWithValue("@kayitkisi", textBox10.Text);
+                cmd.Parameters.AddWithValue("@tarih", dateTimePicker1.Value);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                griddoldur();
+            }
+        Bitis: { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("TC Kimlik Numarası eksik.");
+                goto Bitir;
+            }
+            else
+            {
+                if (textBox6.Text.Length > 254)
+                {
+                    MessageBox.Show("Açıklama 255 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox1.Text.Length != 11)
+                {
+                    MessageBox.Show("TC Kimlik Numarası 11 karakter olmak zorunda.");
+                    goto Bitir;
+                }
+                if (textBox2.Text.Length > 49)
+                {
+                    MessageBox.Show("Ad Soyad 50 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox3.Text.Length > 19)
+                {
+                    MessageBox.Show("Telefon Numarası 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox5.Text.Length > 19)
+                {
+                    MessageBox.Show("Dosya Numarası 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox7.Text.Length > 19)
+                {
+                    MessageBox.Show("Sıra Numarası 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox8.Text.Length > 19)
+                {
+                    MessageBox.Show("Kart Tipi 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox9.Text.Length > 19)
+                {
+                    MessageBox.Show("Kartı Basan kişi 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                if (textBox10.Text.Length > 19)
+                {
+                    MessageBox.Show("Kayıt yapan kişi 20 karakterden fazla olamaz.");
+                    goto Bitir;
+                }
+                goto Update;
+            }
+            Update:
+            {
+                string sorgu = "Update kisi Set adsoyad=@adsoyad,tel=@tel,dosya=@dosya,aciklama=@aciklama,sira=@sira,kart=@kart,basankisi=@basankisi,kayitkisi=@kayitkisi,tarih=@tarih Where no=@no";
+                cmd = new SqlCommand(sorgu, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@no", textBox1.Text);
+                cmd.Parameters.AddWithValue("@adsoyad", textBox2.Text);
+                cmd.Parameters.AddWithValue("@tel", textBox3.Text);
+                cmd.Parameters.AddWithValue("@dosya", textBox5.Text);
+                cmd.Parameters.AddWithValue("@aciklama", textBox6.Text);
+                cmd.Parameters.AddWithValue("@sira", textBox7.Text);
+                cmd.Parameters.AddWithValue("@kart", textBox8.Text);
+                cmd.Parameters.AddWithValue("@basankisi", textBox9.Text);
+                cmd.Parameters.AddWithValue("@kayitkisi", textBox10.Text);
+                cmd.Parameters.AddWithValue("@tarih", dateTimePicker1.Value);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                griddoldur();
+            }
+        Bitir: { }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "delete from kisi where no=" + textBox1.Text + "";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            griddoldur();
+            MessageBox.Show("Kayıt Silindi.");
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            griddoldur();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox7.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            textBox8.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            textBox9.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            textBox10.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=DESKTOP-BAV2LIP\\SQLEXPRESS;Initial Catalog=DENEME;Integrated Security=True");
+            con.Open();
+            string arama = "Select *From kisi where no like '%" + textBox1.Text + "%' and adsoyad like '%" + textBox2.Text + "%' and tel like '%" + textBox3.Text + "%' and dosya like '%" + textBox5.Text + "%'and aciklama like '%" + textBox6.Text + "%'and sira like '%" + textBox7.Text + "%'and kart like '%" + textBox8.Text + "%'and basankisi like '%" + textBox9.Text + "%'and kayitkisi like '%" + textBox10.Text + "%'";           
+            da = new SqlDataAdapter(arama,con);
+            ds = new DataSet();
+            da.Fill(ds, "kisi");
+            dataGridView1.DataSource = ds.Tables["kisi"];
+            con.Close();
+        }
+    }
+}
